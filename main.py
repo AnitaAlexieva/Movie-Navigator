be3b45387e4bfd6f0723c85f3e73cf89de28a5f9
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-10-28 23:51:37 +0200

    Аctivate the style.css file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from controllers import movie_controller
 
 app = FastAPI()
+
+# 1️⃣ Свържи папката "static"
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 2️⃣ Кажи на FastAPI къде са шаблоните
+templates = Jinja2Templates(directory="views")
+
+# 3️⃣ Добави контролера
 app.include_router(movie_controller.router)
